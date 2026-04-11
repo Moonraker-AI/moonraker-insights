290bbb04c42f69df9b9a92a21abec23649e91d2d
Chris Morin <127409240+Chris-Morin@users.noreply.github.com>
2026-04-11 10:27:18 +0700

    Add admin auth to discover-services.js

--- api/discover-services.js
+++ api/discover-services.js
@@ -280,6 +280,7 @@ async function getGoogleAccessToken(saJson) {
       throw new Error('Service account JSON missing private_key or client_email');
     }
     var crypto = require('crypto');
+var auth = require('./_lib/auth');
 
     var header = Buffer.from(JSON.stringify({ alg: 'RS256', typ: 'JWT' })).toString('base64url');
     var now = Math.floor(Date.now() / 1000);
